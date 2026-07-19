48e2b3e1f10f224c7a5f5f46dec059a5382300fa
Louis <jugj@users.noreply.github.com>
2026-07-19 09:18:45 +0200

    Änderungen von Louis am 2026-07-19 09:18:45

--- Assets/Scripts/Louis/SandUhr.cs
+++ Assets/Scripts/Louis/SandUhr.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class SandUhr : MonoBehaviour
 {
-    [SerializeField] private GameObject targetTimer;
-    public enum Timers {Right , Left}
-    [SerializeField] private Timers chosenTimer;
-    public enum Time {1Sekund , 3Sekunden};
+    [SerializeField]  GameObject targetTimer;
+    public enum Time {EineSekunde , DreiSekunden};
     [SerializeField] private Time chosenTime;
     private CircleCollider2D CircleCollider2D;
     private SpriteRenderer SpriteRenderer;
@@ -17,33 +15,34 @@ public class SandUhr : MonoBehaviour
     {
         CircleCollider2D = gameObject.GetComponent<CircleCollider2D>();
         SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        if(chosenTime == Time.1Sekunde)
+        if(chosenTime == Time.EineSekunde)
         {
             SpriteRenderer.sprite = Sprites[0];
         }
-        if(chosenDirection == Timers.Left)
+        if(chosenTime == Time.DreiSekunden)
         {
             SpriteRenderer.sprite = Sprites[1];
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
-        void OnTriggerEnter2D(Collider2D other)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Rotate");
         if  (other.CompareTag("Player"))
         {
             CircleCollider2D.enabled = false;
             SpriteRenderer.enabled = false;
-            if(chosenDirection == Direction.Right)
+            if(chosenTime == Time.EineSekunde)
             {
+                
             }
-            if(chosenDirection == Direction.Left)
+            if(chosenTime == Time.DreiSekunden)
             {
+
             }
         }
     }
