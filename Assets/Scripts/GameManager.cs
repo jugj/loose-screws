e9c846459db4ba5245ca8b662d7e711cb0a91733
Felix <jugj@users.noreply.github.com>
2026-07-19 11:32:40 +0200

    Änderungen von Felix am 2026-07-19 11:32:40

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public GridBasedMovement[] playerNumbers;
     public int winner;
+	private string spielStatus;
     
     public static GameManager Instance { get;private set; }
     
